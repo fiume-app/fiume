d70126ef50b4c5406d3f07a42b9d89e6f9b97a66
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-09-25 10:21:34 +0530

    flutter_native_splash init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 
   await Firebase.initializeApp(
