3dcf673fe64cd44d23a74160158772610f30bdc5
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-09-25 14:52:10 +0530

    Create app.dart

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:fiume/providers/router.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class App extends ConsumerWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+    );
+  }
+}
