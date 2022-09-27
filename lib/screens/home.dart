cbcf0aaa4647135d110af5e192eeeaf062cf661f
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-09-27 20:13:40 +0530

    home screen updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:fiume/providers/user.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class Home extends ConsumerWidget {
@@ -10,20 +11,27 @@ class Home extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: null,
+      appBar: AppBar(
+        title: Text('Fiume'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_bag, size: 30),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle, size: 30),
+            onPressed: () {
+              context.push("/profile");
+            },
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text('Home'),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: ElevatedButton(
-                child: Text('Sign Out'),
-                onPressed: () => ref.read(userProvider.notifier).signout(),
-              ),
-            ),
           ],
         ),
       ),
