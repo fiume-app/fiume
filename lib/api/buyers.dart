14540d6c7c8951bac99ec128c76c776780504397
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-10-06 16:01:23 +0530

    buyers api updated

--- lib/api/buyers.dart
+++ lib/api/buyers.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fiume/models/address.dart';
 import 'package:fiume/models/buyer.dart';
 import 'package:fiume/models/error.dart';
 import 'package:http/http.dart';
@@ -36,6 +37,82 @@ Future<GetOrPostBuyersRet> getOrPostBuyers() async {
   }
 
   return GetOrPostBuyersRet(
+    error: errorObj,
+  );
+}
+
+Future<PostAddressRet> postAddress(PostAddressParams d) async {
+  var idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
+
+  final response = await post(
+    Uri.parse('$_url/addresses'),
+    body: jsonEncode({
+      'line1': d.line1,
+      'line2': d.line2,
+      'city': d.city,
+      'state': d.state,
+      'pin_code': d.pinCode,
+    }),
+    headers: {
+      'Authorization': idToken!,
+      'Content-Type': 'application/json',
+    }
+  );
+
+  if (response.statusCode == 200) {
+    return PostAddressRet(
+      response: Address.fromJSON(jsonDecode(response.body))
+    );
+  }
+
+  ApiErrorV1 errorObj;
+
+  try {
+    errorObj = ApiErrorV1.fromJSON(jsonDecode(response.body));
+  } catch (e) {
+    errorObj = ApiErrorV1(
+      code: 'UNKNOWN_ERROR',
+      msg: 'Something went wrong !',
+      error: '',
+    );
+  }
+
+  return PostAddressRet(
+    error: errorObj,
+  );
+}
+
+Future<GetAddressesRet> getAddresses() async {
+  var idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
+
+  final response = await get(
+    Uri.parse('$_url/addresses'),
+    headers: {
+      'Authorization': idToken!,
+    }
+  );
+
+  if (response.statusCode == 200) {
+    Iterable i = jsonDecode(response.body);
+
+    return GetAddressesRet(
+      response: i.map((e) => Address.fromJSON(e)).toList(),
+    );
+  }
+
+  ApiErrorV1 errorObj;
+
+  try {
+    errorObj = ApiErrorV1.fromJSON(jsonDecode(response.body));
+  } catch (e) {
+    errorObj = ApiErrorV1(
+      code: 'UNKNOWN_ERROR',
+      msg: 'Something went wrong !',
+      error: '',
+    );
+  }
+
+  return GetAddressesRet(
     error: errorObj,
   );
 }
