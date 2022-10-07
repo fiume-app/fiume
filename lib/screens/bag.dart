e63ed51cbb88ee24d722bdfb93e6c6d69fe3a275
Vinayak Nivalkar <57988280+VinayakNivalkar@users.noreply.github.com>
2022-10-07 22:51:32 +0530

    bag screen added

---
+++ lib/screens/bag.dart
@@ -0,0 +1,230 @@
+import 'package:fiume/models/bag.dart';
+import 'package:fiume/models/error.dart';
+import 'package:fiume/providers/bag.dart';
+import 'package:fiume/widgets/error_dialog_v1.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class Bag extends ConsumerWidget {
+  const Bag({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bag'),
+      ),
+      body: ref.watch(bagProvider).when(
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        error: (err, stack) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text((err as ApiErrorV1).msg, style: Theme.of(context).textTheme.titleMedium),
+              Text('ERR_CODE: ${(err).code}', style: Theme.of(context).textTheme.overline),
+              const Padding(padding: EdgeInsets.all(5)),
+              ElevatedButton(
+                child: const Text('Retry'),
+                onPressed: () {
+                  ref.refresh(bagProvider);
+                },
+              )
+            ],
+          ),
+        ),
+        data: (state) => Stack(
+          children: [
+            RefreshIndicator(
+              child: ListView(
+                padding: const EdgeInsets.only(bottom: 120),
+                children: state.bag.map((e) => Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 120,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.grey.shade100,
+                              ),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Image.network('http://127.0.0.1:3003/v1/public/${e.pattern.images[0].sizes.small.filename}'),
+                            ),
+                          ),
+                          Flexible(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    e.product.name,
+                                    style: Theme.of(context).textTheme.titleLarge,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  ),
+                                  Text(
+                                    e.pattern.name,
+                                    style: Theme.of(context).textTheme.titleSmall,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                  ),
+                                  const Padding(padding: EdgeInsets.all(10)),
+                                  Text(
+                                    '₹${e.pattern.price.toStringAsFixed(2)}',
+                                    style: Theme.of(context).textTheme.headlineSmall,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              e.qty > 1 ?
+                              FloatingActionButton(
+                                mini: true,
+                                elevation: 0,
+                                onPressed: () async {
+                                  PatchBagRet ret = await ref.read(bagProvider.notifier).decrementQty(e.id);
+
+                                  if (ret.error != null) {
+                                    showDialog(context: context, builder: (context) => ErrorDialogV1(errorString: ret.error!.msg));
+                                  }
+                                },
+                                child: const Icon(Icons.remove),
+                              )
+                                  :
+                              FloatingActionButton(
+                                mini: true,
+                                elevation: 0,
+                                onPressed: () async {
+                                  DeleteBagRet ret = await ref.read(bagProvider.notifier).deleteItem(e.id);
+
+                                  if (ret.error != null) {
+                                    showDialog(context: context, builder: (context) => ErrorDialogV1(errorString: ret.error!.msg));
+                                  }
+                                },
+                                child: const Icon(Icons.close),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(e.qty.toString(), style: Theme.of(context).textTheme.titleLarge),
+                          ),
+                          Row(
+                            children: [
+                              FloatingActionButton(
+                                mini: true,
+                                elevation: 0,
+                                onPressed: () async {
+                                  PatchBagRet ret = await ref.read(bagProvider.notifier).incrementQty(e.id);
+
+                                  if (ret.error != null) {
+                                    showDialog(context: context, builder: (context) => ErrorDialogV1(errorString: ret.error!.msg));
+                                  }
+                                },
+                                child: const Icon(Icons.add),
+                              )
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )).toList(),
+              ),
+              onRefresh: () async {
+                ref.refresh(bagProvider);
+              },
+            ),
+            Positioned(
+              bottom: 30,
+              left: 20,
+              right: 20,
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                width: double.infinity,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Theme
+                      .of(context)
+                      .colorScheme
+                      .primary,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text('Total: ', style: Theme
+                          .of(context)
+                          .textTheme
+                          .titleMedium
+                          ?.merge(TextStyle(color: Theme
+                          .of(context)
+                          .colorScheme
+                          .inversePrimary))),
+                      Text(
+                        '₹${state.bag.isEmpty ? 0 : state.bag.map((e) => e.pattern.price * e.qty).reduce((value, element) => value + element).toStringAsFixed(2)}',
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.merge(TextStyle(color: Theme
+                            .of(context)
+                            .colorScheme
+                            .inversePrimary))
+                      ),
+                      Spacer(),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          showDialog(context: context, builder: (c) => AlertDialog(
+                            title: Text('Are you Sure?'),
+                            content: Text('This is an irreversible process !'),
+                            actions: [
+                              TextButton(onPressed: () {
+                                Navigator.pop(c);
+                              }, child: Text('No')),
+                              ElevatedButton(onPressed: () {
+                                Navigator.pop(c);
+                                context.push("/order_conf");
+                              }, child: Text('Yes')),
+                            ],
+                          ));
+                        },
+                        label: const Text('Checkout'),
+                        icon: const Icon(Icons.credit_card),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
